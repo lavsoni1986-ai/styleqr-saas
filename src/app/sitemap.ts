7f1d872e620a1e64431126ba6bb70756259d51ec
lavkumarsoni <lavsoni1986@gmail.com>
2026-02-14 20:26:15 +0530

    Added sitemap for Google

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,27 +1,21 @@
-import { MetadataRoute } from "next";
+import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = process.env.NEXTAUTH_URL || "http://localhost:3000";
+  const baseUrl = 'https://stylerqrestaurant.in'
 
   return [
     {
       url: baseUrl,
       lastModified: new Date(),
-      changeFrequency: "monthly",
+      changeFrequency: 'daily',
       priority: 1,
     },
     {
       url: `${baseUrl}/login`,
       lastModified: new Date(),
-      changeFrequency: "monthly",
+      changeFrequency: 'monthly',
       priority: 0.8,
     },
-    {
-      url: `${baseUrl}/signup`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-  ];
+    // Add more pages here if needed
+  ]
 }
-
